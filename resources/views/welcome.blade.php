d12d9bac942f25dd55a9ff7d926a14a70a3ff9fb
Ilham Muttaqien <mt.ilham@gmail.com>
2020-09-15 21:29:31 +0700

    Mengedit routes web dan menambah views blog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rajin Ngoding
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('halo', function () {
+    return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
